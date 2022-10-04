da5d8f388eb5719d0f0655a1061ee0faf78e2e1f
annoinspace <aneesahalmask@gmail.com>
2022-10-04 14:18:56 +0100

    recipe is complete :)
    final steps added and comment block removed since small comments are added at each step

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-/*
-You’re going to create a variable for each step of the recipe, and you’re naming it with a meaningful name representing the recipient used or the step itself.
-In each variable you’re going to sum the quantities of the ingredients used.
-In the end you should have a variable containing the sum of all the ingredients in a variable called “carbonara”
-display the value of carbonara in a console.log()
-Spaghetti alla Carbonara Recipe:
-Ingredients for 4 persons:
-         spaghetti 400g,
-         guanciale 250g,
-         very fresh egg yolks 6,
-         aged grated Pecorino Romano cheese 50g,
-         raw black pepper 4g
-To make classic carbonara, first cut the guanciale into 1cm layers, then into long strips.
-Combine the egg yolks with the finely grated Pecorino Romano.
-Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of it to the yolks and chees mixture, set aside the rest.
-Brown the strips of guanciale for 3 minutes at medium heat, then 1 minute on high heat til crisp, then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down.
-Cook the pasta with a pinch of salt in the water (guanciale is already very salty);
-set aside a ladleful of the pasta water before draining it, then drain the pasta once it is cooked al dente.
-Pour 2 spoons of the reserved hot water into the part of the frying pan containing the cooled guanciale and turn on the heat:
-this will create a creamy sauce with the pasta starch contained in that water.
-TURN OFF THE HEAT AND SET ASIDE for 1 minute, then transfer the pasta to the same pan and mix together.
-Add the yolk and cheese mixture, stirring rapidly. In the warm pan with the hot pasta, the eggs will cook gently and become creamy.
-It is important to do this part quickly to prevent the yolks from congealing and taking on the texture of scrambled eggs.
-Add the remaining roasted black pepper and serve immediately.
-*/
-
 let spaghetti = 400
 let guanciale = 250
 let freshEggYolks = 6
@@ -56,4 +30,53 @@ console.log(
   `After cooking the guanciale we are left with ` +
     cookedGuanciale +
     ` grams of meat`
+)
+
+// cook pasta
+let boilingWater = 2000
+let pinchOfSalt = 1
+let pastaWater = boilingWater + pinchOfSalt
+let potOfPasta = pastaWater + spaghetti
+
+// pasta is now cooked
+let waterAbsorbtionRate = 1.8
+let cookedPasta = spaghetti * waterAbsorbtionRate
+let seasonedCookedPasta = pinchOfSalt * 0.5 + cookedPasta
+let pastaWaterLeft = potOfPasta - cookedPasta
+console.log(
+  `The cooked and drained spaghetti is ` +
+    seasonedCookedPasta +
+    ` and the amount of water left is ` +
+    pastaWaterLeft
+)
+
+// keep aside 1 ladle of pasta water
+let ladleOfPastaWater = 150
+
+// add hot pasta water to cooked guanciale
+let spoonOfPastaWater = 15
+let sauce = cookedGuanciale + spoonOfPastaWater + spoonOfPastaWater
+console.log(`Our pasta sauce is ready: ` + sauce)
+
+// add spaghetti to sauce
+let spaghettiInSauce = sauce + seasonedCookedPasta
+console.log(
+  `After adding out cooked spaghetti to the sauce we have : ` + spaghettiInSauce
+)
+
+// add the yolk and cheese mixture - make sure to stir quickly
+let carbonara = spaghettiInSauce + seasonedEggYolkPecorinoMixture
+console.log(
+  `After adding the yolk mixture and stirring quickly over heat, our carbonara is alomst done :` +
+    carbonara
+)
+
+// the remaining black pepper
+let blackPepperUsed = blackPepper * 0.33
+let blackPepperRemaining = blackPepper - blackPepperUsed
+let finishedDish = carbonara + blackPepperRemaining
+console.log(
+  `Yay! We have finished cooking our carbonara :` +
+    finishedDish +
+    `. All that is left is to eat!`
 )
